6872869ac669908229886f1864486eae9dd19b34
XieZongChen <952937570@qq.com>
2023-12-05 09:30:40 +0800

    feat: Dirty flag

--- src/document.rs
+++ src/document.rs
@@ -8,6 +8,7 @@ use std::{
 pub struct Document {
     rows: Vec<Row>,
     pub file_name: Option<String>,
+    dirty: bool,
 }
 
 impl Document {
@@ -20,6 +21,7 @@ impl Document {
         Ok(Self {
             rows,
             file_name: Some(filename.to_string()),
+            dirty: false,
         })
     }
 
@@ -36,6 +38,10 @@ impl Document {
     }
 
     pub fn insert(&mut self, at: &Position, c: char) {
+        if at.y > self.len() {
+            return;
+        }
+        self.dirty = true;
         if c == '\n' {
             self.insert_newline(at);
             return;
@@ -44,7 +50,7 @@ impl Document {
             let mut row = Row::default();
             row.insert(0, c);
             self.rows.push(row);
-        } else if at.y < self.len() {
+        } else {
             let row = self.rows.get_mut(at.y).unwrap();
             row.insert(at.x, c);
         }
@@ -55,6 +61,7 @@ impl Document {
         if at.y >= len {
             return;
         }
+        self.dirty = true;
         if at.x == self.rows.get_mut(at.y).unwrap().len() && at.y < len - 1 {
             let next_row = self.rows.remove(at.y + 1);
             let row = self.rows.get_mut(at.y).unwrap();
@@ -65,21 +72,23 @@ impl Document {
         }
     }
 
-    pub fn save(&self) -> Result<(), Error> {
+    pub fn save(&mut self) -> Result<(), Error> {
         if let Some(file_name) = &self.file_name {
             let mut file = fs::File::create(file_name)?;
             for row in &self.rows {
                 file.write_all(row.as_bytes())?;
                 file.write_all(b"\n")?;
             }
+            self.dirty = false;
         }
         Ok(())
     }
 
+    pub fn is_dirty(&self) -> bool {
+        self.dirty
+    }
+
     fn insert_newline(&mut self, at: &Position) {
-        if at.y > self.len() {
-            return;
-        }
         if at.y == self.len() {
             self.rows.push(Row::default());
             return;
