f9efa2f804ace018b91af9789ec3d50cf605e9d4
XieZongChen <952937570@qq.com>
2023-12-27 10:09:42 +0800

    feat: Colorful characters

--- src/highlighting.rs
+++ src/highlighting.rs
@@ -5,7 +5,8 @@ pub enum Type {
     None,
     Number,
     Match,
-    String
+    String,
+    Characters
 }
 
 impl Type {
@@ -14,6 +15,7 @@ impl Type {
             Type::Number => color::Rgb(220, 163, 163),
             Type::Match => color::Rgb(38, 139, 210),
             Type::String => color::Rgb(211, 54, 130),
+            Type::Characters => color::Rgb(255, 255, 255),
             _ => color::Rgb(255, 255, 255),
         }
     }
