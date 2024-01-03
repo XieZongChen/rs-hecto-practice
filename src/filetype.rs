1b678cb2fce0210ac6989dbe13565e12fe5ea9af
XieZongChen <952937570@qq.com>
2024-01-03 10:58:15 +0800

    feat: Colorful multi-line comments

--- src/filetype.rs
+++ src/filetype.rs
@@ -9,6 +9,7 @@ pub struct HighlightingOptions {
     strings: bool,
     characters: bool,
     comments: bool,
+    multiline_comments: bool,
     primary_keywords: Vec<String>,
     secondary_keywords: Vec<String>,
 }
@@ -40,6 +41,7 @@ impl FileType {
                     strings: true,
                     characters: true,
                     comments: true,
+                    multiline_comments: true,
                     primary_keywords: vec![
                         "as".to_string(),
                         "break".to_string(),
@@ -128,6 +130,9 @@ impl HighlightingOptions {
     }
     pub fn comments(&self) -> bool {
         self.comments
+    }
+    pub fn multiline_comments(&self) -> bool {
+        self.multiline_comments
     }
     pub fn primary_keywords(&self) -> &Vec<String> {
         &self.primary_keywords
